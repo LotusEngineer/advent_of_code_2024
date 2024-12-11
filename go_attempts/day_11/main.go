dc496fa7374acf0e87b9463552925bb839e870df
Ryan Burke <hello@ryanburke.io>
2024-12-11 14:38:48 +0000

    All of day 11 complete, optimization challenge for the second part was fun

--- go_attempts/day_11/main.go
+++ go_attempts/day_11/main.go
@@ -3,17 +3,90 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
+	"time"
 )
 
 //go:embed input.txt
 var inputString string
 
 func main() {
-	stones := strings.Fields(strings.TrimSpace(inputString))
+	start := time.Now()
+	stonesInput := strings.Fields(strings.TrimSpace(inputString))
+
+	stoneValues := make(map[int]int)
+	for _, s := range stonesInput {
+		stoneValue, _ := strconv.Atoi(s)
+		stoneValues[stoneValue]++
+	}
+
+	transformationCache := make(map[int][]int)
+	transformationCache[0] = []int{1}
 
 	for i := 0; i < 75; i++ {
+		nextStoneValues := make(map[int]int)
+
+		for stoneValue, count := range stoneValues {
+			if _, found := transformationCache[stoneValue]; !found {
+				transformationCache[stoneValue] = transformStone(stoneValue)
+			}
+
+			transformedStones := transformationCache[stoneValue]
+			for _, newStoneValue := range transformedStones {
+				nextStoneValues[newStoneValue] += count
+			}
+		}
+
+		stoneValues = nextStoneValues
+	}
+
+	totalStonesCount := 0
+	for _, count := range stoneValues {
+		totalStonesCount += count
+	}
+
+	elapsed := time.Since(start)
+
+	fmt.Println("Total final stones (75 iterations):", totalStonesCount)
+	fmt.Println("Time taken:", elapsed)
+}
+
+func transformStone(stoneValue int) []int {
+	if stoneValue == 0 {
+		return []int{1}
+	}
+
+	valueLength := digitLength(stoneValue)
+	if valueLength%2 == 0 {
+		halfLength := valueLength / 2
+		tenPower := int(math.Pow10(halfLength))
+
+		leftStoneValue := stoneValue / tenPower
+		rightStoneValue := stoneValue % tenPower
+		return []int{leftStoneValue, rightStoneValue}
+	} else {
+		return []int{stoneValue * 2024}
+	}
+}
+
+func digitLength(n int) int {
+	if n == 0 {
+		return 1
+	}
+	length := 0
+	for n > 0 {
+		n /= 10
+		length++
+	}
+	return length
+}
+
+func Part1() {
+	stones := strings.Fields(strings.TrimSpace(inputString))
+
+	for i := 0; i < 25; i++ {
 		newStones := make([]string, 0, len(stones)*2)
 
 		for _, stone := range stones {
@@ -42,5 +115,5 @@ func main() {
 		stones = newStones
 	}
 
-	fmt.Println("Total final stones:", len(stones))
+	fmt.Println("Total final stones (25 iterations):", len(stones))
 }
